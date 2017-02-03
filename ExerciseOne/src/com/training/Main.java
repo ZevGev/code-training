0cf79fe7115befad45f73c3f588b6f49c86d3215
Zeevi Gev <Zeevi Gev>
2017-02-03 21:03:12 +0200

    First Commit

---
+++ ExerciseOne/src/com/training/Main.java
@@ -0,0 +1,10 @@
+package com.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String newString = new String();
+	    newString = "Hello World";
+        System.out.println(newString);
+    }
+}
